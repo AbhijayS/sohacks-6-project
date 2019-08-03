580ded603d5a5d4c97eca28e045db3e38f9cc39e
Ibraheem Moosa <ibraheemmoosa1@gmail.com>
2019-08-03 08:58:22 -0500

    Added model.sfb

--- app/src/main/java/com/google/ar/sceneform/samples/solarsystem/SolarActivity.java
+++ app/src/main/java/com/google/ar/sceneform/samples/solarsystem/SolarActivity.java
@@ -134,7 +134,7 @@ public class SolarActivity extends AppCompatActivity {
 //        ModelRenderable.builder().setSource(this, Uri.parse("Neptune.sfb")).build();
 
     CompletableFuture<ModelRenderable> markerStage =
-            ModelRenderable.builder().setSource(this, Uri.parse("Golf ball.sfb")).build();
+            ModelRenderable.builder().setSource(this, Uri.parse("model.sfb")).build();
 
     // Build a renderable from a 2D View.
 //    CompletableFuture<ViewRenderable> solarControlsStage =
